14f138860bcfc02c75e0c4afd8ecd2159337df57
Felipe Lopera Valencia <felipe.lopera@MacBook-Pro-de-Administrador.local>
2024-02-01 16:22:16 -0500

    Se agrega recurso para consultar las ubicacions, también se agrega el from

--- airbnb/src/main/java/com/flypass/airbnb/AirbnbApplication.java
+++ airbnb/src/main/java/com/flypass/airbnb/AirbnbApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AirbnbApplication {
@@ -11,4 +13,15 @@ public class AirbnbApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AirbnbApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:9000");
+			}
+		};
+	}
+
 }
